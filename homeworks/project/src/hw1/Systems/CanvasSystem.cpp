a215dc75768b5dcc1713fe1fa3f44fadcfe2035f
chang.liu01 <chang.liu01@united-imaging.com>
2020-11-13 17:29:31 +0800

    up homework

--- homeworks/project/src/hw1/Systems/CanvasSystem.cpp
+++ homeworks/project/src/hw1/Systems/CanvasSystem.cpp
@@ -3,32 +3,193 @@
 #include "../Components/CanvasData.h"
 
 #include <_deps/imgui/imgui.h>
+#include <eigen/Eigen/Dense>
 
 using namespace Ubpa;
+
+#define dataScale 10.0f
+#define sig 5.0
+#define pDim 4
+#define lamda 2
+namespace
+{
+	void FuncPolynomial(float x, Eigen::VectorXf& span)
+	{
+		int dim =(int) span.size();
+		for (int i = 0; i < dim; i++) {
+			span(i) = std::pow(x,  i);
+		}
+	}
+	void ConstructSpanInsertPolynomial(const std::vector<pointf2>& input,size_t dim,Eigen::MatrixXf &m_A)
+	{
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				m_A(i, j) = std::pow(input[i][0], j);
+			}
+		}
+	}
+	void ConstructSpanApproachPolynomial(const std::vector<pointf2>& input, size_t dim, Eigen::MatrixXf& m_A,Eigen::VectorXf &v_b)
+	{		
+		size_t pointNum = input.size();
+		m_A.resize(dim, dim);
+		m_A.setZero();
+		v_b.setZero();
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				for (int k = 0; k < pointNum; k++) {
+					m_A(i, j) += std::pow(input[k][0], i + j);
+				}
+			}
+		}
+		for (int i = 0; i < dim; ++i) {
+			for (int k = 0; k < pointNum; ++k) {
+				v_b(i) += input[k][1] * std::pow(input[k][0], i);
+			}
+		}
+	}
+
+	void AddRestrain(Eigen::MatrixXf& m_A)
+	{
+		//lamda
+		size_t row = m_A.rows();
+		Eigen::MatrixXf mIdenty(row, row);
+		mIdenty.setIdentity();
+		m_A = m_A + (lamda / dataScale) * mIdenty;
+	}
+
+
+	void FuncGause(float x, size_t dim, float sigma,const std::vector<pointf2>& input, Eigen::VectorXf& span)
+	{
+		span(0) = 1;
+		for (int i = 1; i < dim; i++) {
+			span(i) = std::exp(-std::pow((x - input[i][0]), 2) / 2.0f / sigma / sigma);
+		}
+	}
+	void ConstructSpanInsertGause(const std::vector<pointf2>& input, size_t dim, float sigma,Eigen::MatrixXf& m_A)
+	{
+		for (int i = 0; i < dim; i++) {
+			m_A(i, 0) = 1;
+			for (int j = 1; j < dim; j++) {
+				m_A(i, j) = std::exp(-std::pow((input[i][0] - input[j][0]), 2) / 2.0f / sigma / sigma);
+			}
+		}
+	}
+
+	void FitInputDataConnect(const std::vector<pointf2>& input, std::vector<pointf2>& output)
+	{
+		output.clear();
+		for (int n = 0; n < input.size() - 1; ++n) {
+			output.push_back(input[n]);
+			output.push_back(input[n + 1]);
+		}
+	}
+
+	void FitInputDataApproach(const std::vector<pointf2>& input,size_t spanSize,bool notAddRestrain,std::vector<pointf2>& output)
+	{
+		size_t pointNum = input.size();
+		Eigen::MatrixXf m_A(spanSize, spanSize);
+		Eigen::VectorXf v_x(spanSize);
+		Eigen::VectorXf v_b(spanSize);
+		ConstructSpanApproachPolynomial(input, spanSize, m_A,v_b);
+		if (!notAddRestrain)
+		{
+			AddRestrain(m_A);
+		}
+
+		v_x = m_A.lu().solve(v_b);
+		//v_x = m_A.fullPivHouseholderQr().solve(v_b);
+		float coorX = input[0][0] - 5.0f;
+		float maxCoorX = input[pointNum - 1][0] + 5.0f;
+		do {
+			float coorY = 0;
+			Eigen::VectorXf v_span(spanSize);
+			FuncPolynomial(coorX, v_span);
+			//FuncGause(coorX, spanSize, sig, input, v_span);
+			coorY = v_span.dot(v_x);
+
+			output.push_back(pointf2(coorX, coorY));
+			coorX += 2.0f / dataScale;
+		} while (coorX < maxCoorX);
+	}
+
+	void FitInputDataInsert(const std::vector<pointf2>& input,bool choice ,std::vector<pointf2>& output)
+	{
+		size_t pointNum = input.size();
+		Eigen::MatrixXf m_A(pointNum, pointNum);
+		Eigen::VectorXf v_x(pointNum);
+		Eigen::VectorXf v_b(pointNum);
+		if(choice)
+			ConstructSpanInsertPolynomial(input, pointNum, m_A);
+		else
+			ConstructSpanInsertGause(input, pointNum, sig, m_A);
+		for (int i = 0; i < pointNum; i++) {
+			v_b(i) = input[i][1];
+		}
+		//v_x = m_A.fullPivHouseholderQr().solve(v_b);
+		v_x = m_A.lu().solve(v_b);
+		float coorX = input[0][0]-5.0f;
+		float maxCoorX = input[pointNum -1][0] + 5.0f;
+		do{
+			float coorY = 0;
+			Eigen::VectorXf v_span(pointNum);
+			if(choice)
+				FuncPolynomial(coorX, v_span);
+			else
+				FuncGause(coorX, pointNum, sig, input, v_span);
+			coorY = v_span.dot(v_x);
+
+			output.push_back(pointf2(coorX, coorY));
+			coorX += 2.0f/ dataScale;
+		} while (coorX < maxCoorX);		
+	}
+}
 
 void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 	schedule.RegisterCommand([](Ubpa::UECS::World* w) {
 		auto data = w->entityMngr.GetSingleton<CanvasData>();
 		if (!data)
-			return;
-
+			return;		
 		if (ImGui::Begin("Canvas")) {
 			ImGui::Checkbox("Enable grid", &data->opt_enable_grid);
 			ImGui::Checkbox("Enable context menu", &data->opt_enable_context_menu);
+			if(data->opt_enable_insert)
+				ImGui::Checkbox("Insert", &data->opt_enable_insert);
+			else
+				ImGui::Checkbox("Approach", &data->opt_enable_insert);
+			if (data->opt_enable_insert)
+			{
+				if (data->opt_choice_0_or_1)
+					ImGui::Checkbox("Polynomial", &data->opt_choice_0_or_1);
+				else
+					ImGui::Checkbox("Gause", &data->opt_choice_0_or_1);
+			}				
+			else
+			{
+				if (data->opt_choice_0_or_1)
+					ImGui::Checkbox("LeastSquare", &data->opt_choice_0_or_1);
+				else
+					ImGui::Checkbox("RidgeRegression", &data->opt_choice_0_or_1);
+			}
+				
+			
+			if (ImGui::Button("Fit Data")&& data->points.size() >1)
+			{					
+				data->fitted_points.clear();
+				if(data->opt_enable_insert)
+					FitInputDataInsert(data->points, data->opt_choice_0_or_1,data->fitted_points);
+				else
+					FitInputDataApproach(data->points, pDim , data->opt_choice_0_or_1,data->fitted_points);
+			}
+			
+			if (ImGui::Button("Clear Data"))
+			{
+				data->points.clear();
+				data->fitted_points.clear();
+			}
+
 			ImGui::Text("Mouse Left: drag to add lines,\nMouse Right: drag to scroll, click for context menu.");
-
-			// Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
-			// Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
-			// To use a child window instead we could use, e.g:
-			//      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));      // Disable padding
-			//      ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(50, 50, 50, 255));  // Set a background color
-			//      ImGui::BeginChild("canvas", ImVec2(0.0f, 0.0f), true, ImGuiWindowFlags_NoMove);
-			//      ImGui::PopStyleColor();
-			//      ImGui::PopStyleVar();
-			//      [...]
-			//      ImGui::EndChild();
-
-			// Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
+			
+						
 			ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
 			ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
 			if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
@@ -46,14 +207,12 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			const bool is_hovered = ImGui::IsItemHovered(); // Hovered
 			const bool is_active = ImGui::IsItemActive();   // Held
 			const ImVec2 origin(canvas_p0.x + data->scrolling[0], canvas_p0.y + data->scrolling[1]); // Lock scrolled origin
-			const pointf2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+			const pointf2 mouse_pos_in_canvas((io.MousePos.x - origin.x)/ dataScale, (io.MousePos.y - origin.y)/ dataScale);
 
 			// Add first and second point
 			if (is_hovered && !data->adding_line && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
 			{
 				data->points.push_back(mouse_pos_in_canvas);
-				data->points.push_back(mouse_pos_in_canvas);
-				data->adding_line = true;
 			}
 			if (data->adding_line)
 			{
@@ -61,7 +220,6 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 				if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
 					data->adding_line = false;
 			}
-
 			// Pan (we use a zero mouse threshold when there's no context menu)
 			// You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
 			const float mouse_threshold_for_pan = data->opt_enable_context_menu ? -1.0f : 0.0f;
@@ -95,8 +253,15 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 				for (float y = fmodf(data->scrolling[1], GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
 					draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
 			}
-			for (int n = 0; n < data->points.size(); n += 2)
-				draw_list->AddLine(ImVec2(origin.x + data->points[n][0], origin.y + data->points[n][1]), ImVec2(origin.x + data->points[n + 1][0], origin.y + data->points[n + 1][1]), IM_COL32(255, 255, 0, 255), 2.0f);
+			//for (int n = 0; n < data->points.size(); n += 2)
+			//	draw_list->AddLine(ImVec2(origin.x + data->points[n][0], origin.y + data->points[n][1]), ImVec2(origin.x + data->points[n + 1][0], origin.y + data->points[n + 1][1]), IM_COL32(255, 255, 0, 255), 2.0f);
+			for (int n = 0; n < data->points.size(); ++n) {
+				draw_list->AddCircleFilled(ImVec2(origin.x+data->points[n][0] * dataScale, origin.y + data->points[n][1] * dataScale), 3, IM_COL32(255, 0, 0, 255));
+			}
+			for (int n = 0; n < ((int)data->fitted_points.size())-1; ++n){
+				draw_list->AddLine(ImVec2(origin.x + data->fitted_points[n][0]* dataScale, origin.y + data->fitted_points[n][1] * dataScale), ImVec2(origin.x + data->fitted_points[n + 1][0] * dataScale, origin.y + data->fitted_points[n + 1][1] * dataScale), IM_COL32(255, 255, 0, 255), 2.0f);
+			}
+				
 			draw_list->PopClipRect();
 		}
 
